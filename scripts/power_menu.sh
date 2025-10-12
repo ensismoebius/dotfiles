786fca5a564befc7e23935b3c01969ea73f28367
André Furlan <ensismoebius@gmai.com>
2025-10-12 02:58:48 -0300

    Power menu now rocks!

--- scripts/power_menu.sh
+++ scripts/power_menu.sh
@@ -4,7 +4,16 @@
 options="󰤄 Suspend\n⭮ Reboot\n⏻ Shutdown"
 
 # Show wofi menu with power options
-selected=$(echo -e "$options" | wofi --dmenu --cache-file /dev/null --style ~/.config/hypr/wofi/style.css --prompt "Power Menu" --width 200)
+selected=$(echo -e "$options" | wofi --dmenu \
+    --cache-file /dev/null \
+    --style ~/.config/hypr/wofi/style.css \
+    --prompt "Power" \
+    --width 120 \
+    --height 105 \
+    --location center \
+    --lines 3 \
+    --hide-scroll \
+    --columns 1)
 
 # Handle the selection
 case "$selected" in
