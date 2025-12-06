f62e4855186f6567c136db370d605a2b2e3ccc5f
André Furlan <ensismoebius@gmail.com>
2025-12-06 04:01:17 -0300

    Float terminal to special workspace

--- stow/Scripts/ui/window/toggle_floating_terminal.sh
+++ stow/Scripts/ui/window/toggle_floating_terminal.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if hyprctl clients -j | jq -e '.[] | select(.class == "floating-terminal")' > /dev/null; then
+# TODO: Mark: All windows in the special as floating terminal windows, all windows marked with that class must be moved to the special workspace
+
+# if hyprctl clients -j | jq -e '.[] | select(.class == "floating-terminal")' > /dev/null; then
     hyprctl dispatch togglespecialworkspace
-else
-    foot --app-id=floating-terminal
-fi
+# else
+#     foot --app-id=floating-terminal
+# fi
