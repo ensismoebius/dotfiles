d3f7f2f46135a177733b9420ef3dc9019878a18b
André Furlan <ensismoebius@gmail.com>
2025-12-06 05:13:37 -0300

    Deleted orphan files

--- install.sh
+++ install.sh
@@ -58,7 +58,7 @@ if ! command -v yay &> /dev/null; then
 fi
 
 # Install all packages with yay
-yay -S --needed hyprland waybar wofi foot nautilus firefox swww polkit-kde-agent qt5ct qt6ct kvantum papirus-icon-theme ttf-jetbrains-mono noto-fonts ttf-font-awesome network-manager-applet bluez-utils udiskie pipewire-pulse pavucontrol grim slurp wl-clipboard jq zsh hyprcursor wlogout xdg-utils grimblast swaync waypaper catppuccin-cursors-mocha vim ccls swaylock playerctl stow gimp mako
+yay -S --needed hyprland waybar wofi foot nautilus firefox swww polkit-kde-agent qt5ct qt6ct kvantum papirus-icon-theme ttf-jetbrains-mono noto-fonts ttf-font-awesome network-manager-applet bluez-utils udiskie pipewire-pulse pavucontrol grim slurp wl-clipboard jq zsh hyprcursor wlogout xdg-utils grimblast swaync waypaper catppuccin-cursors-mocha vim ccls swaylock playerctl stow gimp mako zenity cliphist inxi flatpak
 
 # Function: ensure murrine engine is installed for GTK2 visual improvements
 install_murrine_if_missing() {
